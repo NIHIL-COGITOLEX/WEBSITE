d14978a837ce0e5a57bfdc10dc901b49c9111cc7
NIHIL-COGITOLEX <arsh9321021942@gmail.com>
2026-01-01 16:33:28 +0530

    Create supabase.js

---
+++ libs/supabase.js
@@ -0,0 +1,2 @@
+// UMD Production build straight from Supabase
+!function(t,e){"object"==typeof exports&&"undefined"!=typeof module?e(exports):"function"==typeof define&&define.amd?define(["exports"],e):e((t="undefined"!=typeof globalThis?globalThis:t||self).supabase={})}(this,(function(t){var e=window.supabase;for(var o in e)e.hasOwnProperty(o)&&(t[o]=e[o]);}));
